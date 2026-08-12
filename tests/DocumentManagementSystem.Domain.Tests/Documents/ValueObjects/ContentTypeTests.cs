87a3ed9f09f32320b1c2f3d64facd48e57a0f9d9
Nadeesha Abeywickrama <anadeesha9@gmail.com>
2026-08-12 14:54:02 +0530

    Add ContentType value object

---
+++ tests/DocumentManagementSystem.Domain.Tests/Documents/ValueObjects/ContentTypeTests.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentManagementSystem.Domain.Tests.Documents.ValueObjects;
+
+internal class ContentTypeTests
+{
+}
